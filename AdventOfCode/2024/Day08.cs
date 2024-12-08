5efbc1148963133598f12eda5b827d1beae90c2b
Noah Heber - KNS <guyingreydaboss@gmail.com>
2024-12-08 16:29:24 -0700

    AoC

---
+++ AdventOfCode/2024/Day08.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCodeSupport;
+
+namespace AdventOfCode2024._2024;
+internal class Day08 : AdventBase
+{
+    protected override object InternalPart1()
+    {
+        return "P1";
+    }
+
+    protected override object InternalPart2()
+    {
+        return "P2";
+    }
+}
---
+++ AdventOfCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using AdventOfCodeSupport;
+
+namespace AdventOfCode;
+
+internal class Program
+{
+    static void Main(/*string[] args*/)
+    {
+        Console.WriteLine("Hello, World!");
+
+        var solutions = new AdventSolutions();
+        solutions.GetDay(2024, 8).Benchmark();
+    }
+}
