f2a4e3acfeec325001f97d821a6b808385783907
Noah Heber - KNS <guyingreydaboss@gmail.com>
2024-12-08 17:03:09 -0700

    Program.cs

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -4,12 +4,62 @@ namespace AdventOfCode;
 
 internal class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            var days = new AdventSolutions();
 
-        var solutions = new AdventSolutions();
-        var day = solutions.GetDay(2024, 08);
-        day.Benchmark();
+            var i = 0;
+            Console.WriteLine("Select a day, 'all' to benchmark all, or Control+C to exit:");
+            foreach (var day in days)
+            {
+                Console.ForegroundColor = i % 2 == 0 ? ConsoleColor.Red : ConsoleColor.Green;
+                Console.WriteLine($"{day.Year}-{day.Day}");
+                Console.ForegroundColor = ConsoleColor.White;
+                i++;
+            }
+
+            Console.Write("> ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            var input = Console.ReadLine()!.ToLower().Trim();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            if (input == "all")
+            {
+                days.BenchmarkAll();
+                continue;
+            }
+
+            var selected = days.FirstOrDefault(d => input == $"{d.Year}-{d.Day}");
+            if (selected is null)
+            {
+                Console.WriteLine("Your argument is invalid.");
+                continue;
+            }
+
+            ProcessMethod(selected);
+        }
+    }
+
+    static void ProcessMethod(AdventBase ab)
+    {
+        Console.WriteLine($"Day {ab.Year}-{ab.Day} selected.");
+
+        ab.Part1().Part2();
+
+        Console.WriteLine("Benchmark? (y/n)");
+        Console.Write("> ");
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        var option = Console.ReadKey();
+        Console.WriteLine("\n");
+        Console.ForegroundColor = ConsoleColor.White;
+        if (option.Key != ConsoleKey.Y)
+        {
+            return;
+        }
+
+        ab.Benchmark();
     }
 }
